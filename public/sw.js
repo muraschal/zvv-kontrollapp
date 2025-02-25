de8e85609f5e35890ca2c1f40d0450d1e6c0e42f
muraschal <muraschal@users.noreply.github.com>
2025-02-25 13:28:32 +0100

    chore: Remove iOS native files
    - Clean up repository
    - Focus on web-only PWA implementation
    - Remove unused mobile native code

--- public/sw.js
+++ public/sw.js
@@ -17,6 +17,10 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
